0420a370b2953acb23ba90d10e3f1a4d51a416d6
Tiago Murakami <trmurakami@gmail.com>
2016-05-20 21:09:51 -0300

    Tirar data da instituição de colaboração

--- sh/run_colaboracao.sh
+++ sh/run_colaboracao.sh
@@ -2,7 +2,8 @@
 
 rm ../data/colab_instituicao.csv
 sleep 2
-mongoexport --db sibi --collection producao --type=csv --fields _id,colab_instituicao_trab --query '{ year: { $gte: "2010", $lte: "2015" }, colab_instituicao_trab: { $exists: true } }' --out ../data/colab_instituicao.csv
+#mongoexport --db sibi --collection producao --type=csv --fields _id,colab_instituicao_trab --query '{ year: { $gte: "2010", $lte: "2015" }, colab_instituicao_trab: { $exists: true } }' --out ../data/colab_instituicao.csv
+mongoexport --db sibi --collection producao --type=csv --fields _id,colab_instituicao_trab --query '{ colab_instituicao_trab: { $exists: true } }' --out ../data/colab_instituicao.csv
 sleep 2
 echo 'db.producao.update({},{$unset: {colab_instituicao_corrigido:1}},false,true)' | mongo sibi
 sleep 2
